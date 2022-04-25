242c493abf8437cf40f02ee30794bef8500f7f26
useragents <103281345+useragents@users.noreply.github.com>
2022-04-25 19:50:44 +0100

    Update follow_bot.py

--- follow_bot.py
+++ follow_bot.py
@@ -1,13 +1,11 @@
-try:
-    import requests, random, string
-except ImportError:
-    input("Error while importing modules. Please install the modules in requirements.txt")
+import requests, random, string
 
 class spotify:
 
-    def __init__(self, profile):
+    def __init__(self, profile, proxy = None):
         self.session = requests.Session()
         self.profile = profile
+        self.proxy = proxy
     
     def register_account(self):
         headers = {
@@ -18,20 +16,22 @@ class spotify:
         }
         email = ("").join(random.choices(string.ascii_letters + string.digits, k = 8)) + "@gmail.com"
         password = ("").join(random.choices(string.ascii_letters + string.digits, k = 8))
+        proxies = None
+        if self.proxy != None:
+            proxies = {"https": f"http://{self.proxy}"}
         data = f"birth_day=1&birth_month=01&birth_year=1970&collect_personal_info=undefined&creation_flow=&creation_point=https://www.spotify.com/uk/&displayname=github.com/useragents&email={email}&gender=neutral&iagree=1&key=a1e486e2729f46d6bb368d6b2bcda326&password={password}&password_repeat={password}&platform=www&referrer=&send-email=1&thirdpartyemail=0&fb=0"
         try:
-            create = self.session.post(
-                "https://spclient.wg.spotify.com/signup/public/v1/account",
-                headers = headers,
-                data = data
-            )
-            login_token = create.json()['login_token']
-            with open("Created.txt", "a") as f:
-                f.write(f'{email}:{password}:{login_token}\n')
-            return login_token
+            create = self.session.post("https://spclient.wg.spotify.com/signup/public/v1/account", headers = headers, data = data, proxies = proxies)
+            if "login_token" in create.text:
+                login_token = create.json()['login_token']
+                with open("Created.txt", "a") as f:
+                    f.write(f'{email}:{password}:{login_token}\n')
+                return login_token
+            else:
+                return None
         except:
-            return None
-        
+            return False
+
     def get_csrf_token(self):
         try:
             r = self.session.get("https://www.spotify.com/uk/signup/?forward_url=https://accounts.spotify.com/en/status&sp_t_counter=1")
@@ -74,7 +74,9 @@ class spotify:
             self.profile = self.profile.split("?")[0]
         login_token = self.register_account()
         if login_token == None:
-            return None, "while registering"
+            return None, "while registering, ratelimit"
+        elif login_token == False:
+            return None, f"bad proxy on register {self.proxy}"
         auth_token = self.get_token(login_token)
         if auth_token == None:
             return None, "while getting auth token"
@@ -96,3 +98,5 @@ class spotify:
             return True, None
         except:
             return False, "while following"
+
+#Developed by https://github.com/useragents
