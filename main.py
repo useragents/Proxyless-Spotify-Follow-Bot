2567dd035f757ba99be4baaaa28f0e3f70615007
useragents <103281345+useragents@users.noreply.github.com>
2022-04-25 19:51:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from follow_bot import spotify
-import threading
+import threading, os, time
 
-spotify_profile = "https://open.spotify.com/user/..." #Link or username to profile
 lock = threading.Lock()
-threads = int(input("\nThreads: "))
 counter = 0
+proxies = []
+proxy_counter = 0
+spotify_profile = str(input("Spotify Link or Username: "))
+threads = int(input("\nThreads: "))
+
+def load_proxies():
+    if not os.path.exists("proxies.txt"):
+        print("\nFile proxies.txt not found")
+        time.sleep(10)
+        os._exit(0)
+    with open("proxies.txt", "r", encoding = "UTF-8") as f:
+        for line in f.readlines():
+            line = line.replace("\n", "")
+            proxies.append(line)
+        if not len(proxies):
+            print("\nNo proxies loaded in proxies.txt")
+            time.sleep(10)
+            os._exit(0)
+
+print("\n[1] Proxies\n[2] Proxyless")
+option = int(input("\n> "))
+if option == 1:
+    load_proxies()
 
 def safe_print(arg):
     lock.acquire()
@@ -13,17 +34,23 @@ def safe_print(arg):
 
 def thread_starter():
     global counter
-    obj = spotify(spotify_profile)
-    result = obj.follow()
-    if result:
+    if option == 1:
+        obj = spotify(spotify_profile, proxies[proxy_counter])
+    else:
+        obj = spotify(spotify_profile)
+    result, error = obj.follow()
+    if result == True:
         counter += 1
         safe_print("Followed {}".format(counter))
     else:
-        safe_print("Error")
+        safe_print(f"Error {error}")
 
 while True:
     if threading.active_count() <= threads:
         try:
             threading.Thread(target = thread_starter).start()
+            proxy_counter += 1
         except:
             pass
+        if len(proxies) <= proxy_counter: #Loops through proxy file
+            proxy_counter = 0
